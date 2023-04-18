1c8fca4991d191faeac6d24a74d729ce483f1cff
Diabene <addodiabene69@gmail.com>
2023-04-18 14:12:08 +0000

    added comments

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,27 @@ function changeSlide(index: number) {
   const slide = sliders.find((_, i) => i === index)!;
 
   sliders.filter((_, i) => i !== index).forEach(slide => {
-    slide.children[0].classList.remove('animate');
+    // slide.children[0].classList.remove('animate');
     slide.setAttribute('data-active', 'false');
   })
 
   const parentRect = getElement('.parent').getBoundingClientRect();
-  slide.children[0].classList.add('animate');
+  // slide.children[0].classList.add('animate');
   slide.setAttribute('data-active', 'true');
   const { left, right } = slide.getBoundingClientRect();
-  const elInParent = ( right < parentRect.right) && (left > parentRect.left);
+  const elInParent = ( right < parentRect.right) && (left > parentRect.left); // condition for active slide to be in view
+
+  /* The x distance of the translation of the slides depends on the number of slides to the left or right
+  of the active slide. The multiplier determines the extent to which the slides are translated.
+  */
 
   if(!elInParent) {
-    let multiplier;
+    let multiplier; 
     let remainingEl;
+    /* 
+      If the active slide is not fully in view then either right > parentRect.right or left < parentRect.left
+      In this case it can never be both.
+    */
     if(right > parentRect.right) {
       remainingEl = sliders.slice(sliders.indexOf(slide) + 1).length;
       multiplier = computeMultiple(remainingEl, 'right');
@@ -40,12 +48,13 @@ function changeSlide(index: number) {
   }     
 }
 
-changeSlide(0)
+changeSlide(0) //set the first slide as the active slide by default
+
 arrowBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     const type = btn.classList[1];
     if(type === 'left') {
-      CURRENT_INDEX > 0 && CURRENT_INDEX--
+      CURRENT_INDEX > 0 && CURRENT_INDEX--;
     } else {
       CURRENT_INDEX < sliders.length - 1 && CURRENT_INDEX++;
     }
