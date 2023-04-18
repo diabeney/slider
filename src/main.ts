5bfd460f3df538822f2291b7205b9e17d42b7870
Diabene <addodiabene69@gmail.com>
2023-04-18 13:49:08 +0000

    final

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,55 @@
 import { getAllElements, getElement, isActiveSlide} from "./utils/utils";
+import { computeMultiple } from "./utils/utils";
 
 const arrowBtns = getAllElements<HTMLButtonElement>('.arrow');
 const sliders = getAllElements('.slide');
-let currentIndex = 0;
 
-let slideCounter = 0;
+let CURRENT_INDEX = 0;
 
-function betterChangeSlide(index: number) {
+let distance = 0;
+
+function changeSlide(index: number) {
   const sliderwrapper = getElement('.slider-wrapper');
-  const slide = sliders.find((_, i) => i === index)!
-  sliders.filter((_, i) => i !== index).forEach(slide => slide.setAttribute('data-active', 'false'));
+  const slide = sliders.find((_, i) => i === index)!;
+
+  sliders.filter((_, i) => i !== index).forEach(slide => {
+    slide.children[0].classList.remove('animate');
+    slide.setAttribute('data-active', 'false');
+  })
+
   const parentRect = getElement('.parent').getBoundingClientRect();
+  slide.children[0].classList.add('animate');
   slide.setAttribute('data-active', 'true');
   const { left, right } = slide.getBoundingClientRect();
   const elInParent = ( right < parentRect.right) && (left > parentRect.left);
+
   if(!elInParent) {
+    let multiplier;
+    let remainingEl;
     if(right > parentRect.right) {
-      let remainingEl = sliders.slice(sliders.indexOf(slide) + 1).length;
-      let multiplier = 0;
-      switch (remainingEl) {
-        case 5:
-          multiplier = 1;
-          break;
-        case 4:
-          multiplier = 2;
-          break;
-        case 3:
-          multiplier = 3;
-          break;
-        case 2:
-          multiplier = 4;
-          break;
-        case 1:
-          multiplier = 5;
-          break;
-        default:
-          multiplier = 1;
-        
-      }
-      let distance = (( parentRect.right - right ) - 16 - (67 * multiplier));
-      console.log(distance);
+      remainingEl = sliders.slice(sliders.indexOf(slide) + 1).length;
+      multiplier = computeMultiple(remainingEl, 'right');
+      distance = (( parentRect.right - right ) - (67 * multiplier));
       sliderwrapper.style.transform = `translateX(${distance}px)`;
     } else {
-      let distance = ((parentRect.left - left ) + 16);
+      remainingEl = sliders.slice(0, sliders.indexOf(slide) + 1).length;
+      multiplier = computeMultiple(remainingEl, 'left');
+      distance += (67 * multiplier);
       sliderwrapper.style.transform = `translateX(${distance}px)`;
     }
   }     
 }
 
-betterChangeSlide(0)
+changeSlide(0)
 arrowBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     const type = btn.classList[1];
     if(type === 'left') {
-      currentIndex > 0 && currentIndex--
+      CURRENT_INDEX > 0 && CURRENT_INDEX--
     } else {
-      currentIndex < sliders.length - 1 && currentIndex++;
+      CURRENT_INDEX < sliders.length - 1 && CURRENT_INDEX++;
     }
-    betterChangeSlide(currentIndex);
+    changeSlide(CURRENT_INDEX);
   })
 })
 
@@ -75,8 +67,8 @@ sliders.forEach(slide => {
 
 sliders.forEach(slide => {
   slide.addEventListener('click', () => {
-    !isActiveSlide(slide) && betterChangeSlide(sliders.indexOf(slide));
-    currentIndex = sliders.indexOf(slide)
+    !isActiveSlide(slide) && changeSlide(sliders.indexOf(slide));
+    CURRENT_INDEX = sliders.indexOf(slide)
   })
 })
 
